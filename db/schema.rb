0c4dac6d886773b44096bfdb1811b1fbf882780f
limdan7 <limdan7@berkeley.edu>
2017-01-07 15:32:57 +0000

    create table and users model

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170106063703) do
+ActiveRecord::Schema.define(version: 20170107152838) do
 
   create_table "rooms", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
