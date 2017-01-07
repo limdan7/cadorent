95e3940f17335530eba7812222b3ac762ba74226
limdan7 <limdan7@berkeley.edu>
2017-01-07 06:59:31 +0000

    Basic form and CRUD function

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,50 @@
+class RoomsController < ApplicationController
+  before_action :set_room, only: [:edit,:update,:show,:destroy]
+  
+  def index
+    @rooms = Room.all
+  end
+  
+  def new
+    @room = Room.new
+  end
+  
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      flash[:notice]="The post was successfully created"
+      redirect_to room_path(@room)
+    else
+      render 'new'
+    end
+  end
+  
+  def show
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @room.update(room_params)
+      flash[:notice] = "The post was successfully updated"
+      redirect_to room_path(@room)
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @room.destroy
+    flash[:notice] = "The post was successfully deleted"
+    redirect_to rooms_path
+  end
+
+  private
+    def set_room
+      @room = Room.find(params[:id])
+    end
+    def room_params
+      params.require(:room).permit(:title,:description)
+    end
+end
