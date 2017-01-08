fcc6da8f30bee184929ecc6d8cfb692868eaf95d
limdan7 <limdan7@berkeley.edu>
2017-01-08 06:18:30 +0000

    Admin feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 
 before_action :set_user, only: [:edit, :update, :show]
 before_action :require_same_user, only: [:edit, :update]
+before_action :require_admin, only: [:destroy]
 
   def new
     @user = User.new
@@ -14,8 +15,9 @@ before_action :require_same_user, only: [:edit, :update]
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to Cadorent #{@user.username}"
-      redirect_to rooms_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end
@@ -37,6 +39,13 @@ before_action :require_same_user, only: [:edit, :update]
     end
   end
   
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:danger] = "User and all the articles from the user have been deleted."
+    redirect_to users_path
+  end
+  
   private
     def set_user
       @user = User.find(params[:id])
@@ -47,8 +56,15 @@ before_action :require_same_user, only: [:edit, :update]
     end
     
     def require_same_user
-      if current_user != @user
+      if current_user != @user and !current_user.admin?
         flash[:danger]="You can only edit your profile."
+        redirect_to root_path
+      end
+    end
+    
+    def require_admin
+      if logged_in? and !current_user.admin?
+        flash[:danger] = "Only admin users can perform that action"
         redirect_to root_path
       end
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170107201115) do
+ActiveRecord::Schema.define(version: 20170108060315) do
 
   create_table "rooms", force: :cascade do |t|
     t.string   "title"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20170107201115) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.boolean  "admin",           default: false
   end
 
 end
