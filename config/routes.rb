4ea8a0739947516fc6e6bf02b3876973b5860979
limdan7 <limdan7@berkeley.edu>
2017-01-08 06:37:43 +0000

    Categories model

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   root 'pages#home'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-  
+  resources :categories, except: [:destroy]
   resources :rooms
   resources :users, except: [:new]
   get 'pages/about', to: 'pages#about'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170108060315) do
+ActiveRecord::Schema.define(version: 20170108062859) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "rooms", force: :cascade do |t|
     t.string   "title"
